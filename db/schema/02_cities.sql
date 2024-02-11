497434192505c267fed1c5dc1eecf45f9efdfa56
Caitlin Croteau <cjcroteau@gmail.com>
2024-02-10 21:26:05 -0800

    fix error in cities.sql

--- db/schema/02_cities.sql
+++ db/schema/02_cities.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS cities;
 CREATE TABLE cities (
   city_id SERIAL NOT NULL,
   city_name VARCHAR(255) NOT NULL,
-  health_authrity_id INTEGER,
+  health_authority_name INTEGER,
   PRIMARY KEY (city_id),
-  FOREIGN KEY (heath_authority_id) REFERNCES health_authorities(heath_authority_id)
+  FOREIGN KEY (heath_authority_name) REFERNCES health_authorities(heath_authority_name)
   );
