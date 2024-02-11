1f0ee8fba7731cf34b3ba3f1af39ed2a41a785c8
Caitlin Croteau <cjcroteau@gmail.com>
2024-02-10 21:59:07 -0800

    add components folder

---
+++ codingchallenge/src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+
+
+export default function SearchBar(props) {
+  return (
+    <>
+      <Form>
+        <Form.Group controlId="searchBar">
+          <Form.Control
+            type="text"
+            placeholder={props.placeholder}
+            onChange={props.searchPlant}
+          />
+        </Form.Group>
+      </Form>
+    </>
+  );
+}
