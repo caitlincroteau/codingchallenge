ad32a347396b68ae8798f530e186a74cee94f481
Caitlin Croteau <cjcroteau@gmail.com>
2024-02-10 17:01:16 -0800

    populate clients.sql

---
+++ schema/01_clients.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE IF EXISTS clients CASCADE;
+
+CREATE TABLE clients (
+  client_id SERIAL PRIMARY KEY NOT NULL,
+  year DATE NOT NULL,
+  active BOOLEAN NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  gender VARCHAR(255) NOT NULL,
+  dob DATE NOT NULL,
+  indigenous BOOLEAN NOT NULL,
+  pwd BOOLEAN NOT NULL,
+  vet	BOOLEAN NOT NULL,
+  emergency_sheltered BOOLEAN NOT NULL,
+  bus_pass BOOLEAN NOT NULL,
+  clothing_supplement BOOLEAN NOT NULL,
+  pet_deposit BOOLEAN NOT NULL,
+  pssg BOOLEAN NOT NULL,
+  status VARCHAR(255),
+  deceased BOOLEAN NOT NULL,
+  city_id INTEGER REFERENCES cities(id)
+)
