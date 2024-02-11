cab5b135bfcada813260e36793c139911179395b
Caitlin Croteau <cjcroteau@gmail.com>
2024-02-10 17:16:06 -0800

    add notes.sql

--- schema/02_cities.sql
+++ schema/02_cities.sql
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS cities CASCADE;
 CREATE TABLE cities (
   city_id SERIAL PRIMARY KEY NOT NULL,
   city_name VARCHAR(255) NOT NULL,
-  heath_authority_id INTEGER REFERNCES health_authority(id)
+  heath_authority_id INTEGER REFERNCES health_authorities(id)
 );
---
+++ schema/04_notes.sql
@@ -0,0 +1,9 @@
+
+
+DROP TABLE IF EXISTS notes CASCADE;
+
+CREATE TABLE notes (
+  note_id SERIAL PRIMARY KEY NOT NULL,
+  note_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  client_id INTEGER REFERENCES clients(id)
+)
