2726b5f2dbafb4ab46edf9d2da78cfd0101bdfd7
Caitlin Croteau <cjcroteau@gmail.com>
2024-02-10 17:26:10 -0800

    update clients.sql

--- schema/01_clients.sql
+++ schema/01_clients.sql
@@ -2,13 +2,14 @@
 DROP TABLE IF EXISTS clients CASCADE;
 
 CREATE TABLE clients (
+  year INTEGER NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT 1,
   client_id SERIAL PRIMARY KEY NOT NULL,
-  year_created DATE NOT NULL DEFAULT CURRENT_DATE,
-  active BOOLEAN NOT NULL DEFAULT TRUE,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   gender VARCHAR(255) NOT NULL,
   dob DATE NOT NULL,
+  city_name INTEGER REFERENCES cities(city_name),
   indigenous BOOLEAN NOT NULL,
   pwd BOOLEAN NOT NULL,
   vet	BOOLEAN NOT NULL,
@@ -18,6 +19,6 @@ CREATE TABLE clients (
   pet_deposit BOOLEAN NOT NULL,
   pssg BOOLEAN NOT NULL,
   status VARCHAR(255),
-  deceased BOOLEAN NOT NULL,
-  city_id INTEGER REFERENCES cities(id)
+  deceased DATE
+  
 );
